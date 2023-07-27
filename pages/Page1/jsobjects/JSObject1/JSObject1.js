96e3689d58d655aeafbfba857e2ef714fc03a734
Favour Ohanekwu <favour@appsmith.com>
2023-07-27 22:59:54 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		console.log("test")
+		console.log("test")
+		console.log("test")
+		console.log("test")
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
